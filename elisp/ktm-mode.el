3ee12a18a51ec5b7fcb582060e7d6ab77183f60a
kototama <kototamo@gmail.com>
2013-01-07 16:00:57 +0100

    rotate window code

--- elisp/ktm-mode.el
+++ elisp/ktm-mode.el
@@ -13,6 +13,30 @@
         (goto-line (read-number "Goto line: ")))
     (linum-mode -1)))
 
+(defun rotate-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond ((not (> (count-windows)1))
+         (message "You can't rotate a single window!"))
+        (t
+         (setq i 1)
+         (setq numWindows (count-windows))
+         (while  (< i numWindows)
+           (let* (
+                  (w1 (elt (window-list) i))
+                  (w2 (elt (window-list) (+ (% i numWindows) 1)))
+
+                  (b1 (window-buffer w1))
+                  (b2 (window-buffer w2))
+
+                  (s1 (window-start w1))
+                  (s2 (window-start w2))
+                  )
+             (set-window-buffer w1  b2)
+             (set-window-buffer w2 b1)
+             (set-window-start w1 s2)
+             (set-window-start w2 s1)
+             (setq i (1+ i))))))) 
 
 (define-minor-mode ktm-mode
   "Ktm mode"
@@ -23,6 +47,7 @@
     ;; (define-key keymap (kbd "C-<backspace>") 'forward-char)
     
     (define-key keymap (kbd "C-x C-r") 'rename-current-buffer-file)
+    (define-key keymap (kbd "C-x r r") 'rotate-windows)
     (define-key keymap (kbd "<C-S-down>") 'move-line-down)
     (define-key keymap (kbd "<C-S-up>") 'move-line-up)
     (define-key keymap (kbd "M-o") 'my-helm)

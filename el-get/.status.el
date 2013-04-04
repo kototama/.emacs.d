83862748e218172c9a90bd847b71e9c6b96f2c54
kototama <kototamo@gmail.com>
2013-04-04 11:46:26 +0200

    remove module and commit el-get status

---
+++ el-get/.status.el
@@ -0,0 +1,37 @@
+((clojure-mode status "installed" recipe
+               (:name clojure-mode :website "https://github.com/technomancy/clojure-mode" :description "Emacs support for the Clojure language." :type github :pkgname "technomancy/clojure-mode"))
+ (el-get status "installed" recipe
+         (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
+ (helm status "installed" recipe
+       (:name helm :description "Emacs incremental and narrowing framework" :type github :pkgname "emacs-helm/helm" :features helm-config))
+ (org-mode status "installed" recipe
+           (:name org-mode :website "http://orgmode.org/" :description "Org-mode is for keeping notes, maintaining ToDo lists, doing project planning, and authoring with a fast and effective plain-text system." :type git :url "git://orgmode.org/org-mode.git" :info "doc" :build/berkeley-unix `,(mapcar
+                                                                                                                                                                                                                                                                                                       (lambda
+                                                                                                                                                                                                                                                                                                         (target)
+                                                                                                                                                                                                                                                                                                         (list "gmake" target
+                                                                                                                                                                                                                                                                                                               (concat "EMACS="
+                                                                                                                                                                                                                                                                                                                       (shell-quote-argument el-get-emacs))))
+                                                                                                                                                                                                                                                                                                       '("oldorg"))
+                  :build `,(mapcar
+                            (lambda
+                              (target)
+                              (list "make" target
+                                    (concat "EMACS="
+                                            (shell-quote-argument el-get-emacs))))
+                            '("oldorg"))
+                  :load-path
+                  ("." "lisp" "contrib/lisp")))
+ (paredit status "installed" recipe
+          (:name paredit :description "Minor mode for editing parentheses" :type http :prepare
+                 (progn
+                   (autoload 'enable-paredit-mode "paredit")
+                   (autoload 'disable-paredit-mode "paredit"))
+                 :url "http://mumble.net/~campbell/emacs/paredit.el" :bind
+                 (("C-S-d" . paredit-duplicate-after-point)
+                  ("C-k" . paredit-eager-kill-line)
+                  ("M-R" . paredit-raise-sexp)
+                  ("C-M-a" . paredit-backward)
+                  ("C-M-S-a" . beginning-of-defun))
+                 :symbol paredit))
+ (use-package status "installed" recipe
+              (:name use-package :type github :description "A use-package declaration for simplifying your .emacs" :pkgname "jwiegley/use-package")))

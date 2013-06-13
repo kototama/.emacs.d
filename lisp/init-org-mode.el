3ad64d536e4dbf9ba25e0bd4286460a04f1c9b0f
kototama <kototamo@gmail.com>
2013-06-13 14:28:11 +0200

    org conf

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -2,29 +2,33 @@
 
 (use-package org
   :init (progn
+
+          (defun my-org-mode-hook
+            ()
+            (setq org-file-apps
+                  (append '(("\\.pdf\\'" . "acroread %s")) org-file-apps))
+            (setq org-agenda-span 128)
+            ;; (setq org-agenda-include-diary nil)
+            (define-key org-mode-map (kbd "<C-return>") nil)
+            (define-key org-mode-map (kbd "<return>") 'newline)
+            (define-key org-mode-map (kbd "<C-tab>") nil)
+            (define-key org-mode-map (kbd "<S-iso-lefttab>") nil)
+            (define-key org-mode-map (kbd "<backtab>")
+              nil)
+
+            (use-package ox-reveal
+              :init
+              (progn
+                (setq org-reveal-root "file:///home/pal/Documents/Presentations/reveal.js-master"))))
+
           (setq org-default-notes-file "~/Documents/Notes/todo.org")
 
-          (add-hook 'org-mode-hook
-                    '(lambda ()
-                       (setq org-file-apps
-                             (append '(("\\.pdf\\'" . "acroread %s")) org-file-apps))
-                       (setq org-agenda-span 128)
-                       ;; (setq org-agenda-include-diary nil)
-                       (define-key org-mode-map (kbd "<C-return>") nil)
-                       (define-key org-mode-map (kbd "<return>") 'newline)
-                       (define-key org-mode-map (kbd "<C-tab>") nil)
-                       (define-key org-mode-map (kbd "<S-iso-lefttab>") nil)
-                       (define-key org-mode-map (kbd "<backtab>")
-                         nil)))
-
+          (add-hook 'org-mode-hook 'my-org-mode-hook)
 
           (setq org-agenda-files '("~/Documents/Notes/"))
           (setq org-agenda-diary-file "~/Documents/Notes/diary.org")
           (setq org-agenda-include-diary nil)
 
-	  ;; (use-package ox-reveal)
-          ;; (require 'ox-reveal)
-          ;; (setq org-reveal-root "file:///home/pal/Documents/Presentations/reveal.js-master")
 ))
 
 ;; (setq org-todo-keywords

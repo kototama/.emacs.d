8d4ef1070ccf1ac6202fd38428f65e6f77d12c5f
Kototama <kototamo@gmail.com>
2014-02-25 16:25:50 +0100

    adds org shortcuts

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -1,5 +1,6 @@
+(use-package org
+  :bind (("C-c o a" . org-agenda)
+         ("C-c o o" . org-open-at-point)))
+
 (use-package org-capture
   :bind (("C-c o r" . org-capture)))
-
-(use-package org
-  :bind (("C-c o a" . org-agenda)))

41db9097670b55c3664a8ccb80c7f557819fd5fc
Kototama <kototamo@gmail.com>
2014-06-25 11:42:32 +0200

    use bind-key* to bind keys

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -1,22 +1,15 @@
-(setq user-key-bindings
-      '(("C-S-j" . join-line)
-        ("M-n" . forward-paragraph)
-        ("M-p" . backward-paragraph)
-        ("M-o" . other-window)
-        ("M-S-o" . (lambda ()
-                     (interactive)
-                     (other-window -1)))
-        ("C-S-k" . (lambda ()
-                     (interactive)
-                     (kill-buffer (current-buffer))))
-        ("M-g m" . pop-to-mark-command)))
+(require 'bind-key)
 
-(defun install-keybindings
-  ()
-  "Installs the key bindings defined in user-bindings"
-  (dolist (binding user-key-bindings)
-    (global-set-key (kbd (car binding)) (cdr binding))))
-
-(install-keybindings)
+(bind-key* "C-S-j"  'join-line)
+(bind-key* "M-n"  'forward-paragraph)
+(bind-key* "M-p"  'backward-paragraph)
+(bind-key* "M-o"  'other-window)
+(bind-key* "M-S-o"  (lambda ()
+                       (interactive)
+                       (other-window -1)))
+(bind-key* "C-S-k"  (lambda ()
+                       (interactive)
+                       (kill-buffer (current-buffer))))'
+(bind-key* "M-g m"  'pop-to-mark-command)
 
 (provide 'keybindings)
